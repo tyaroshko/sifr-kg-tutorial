a620b8070954c2902a9e16467e6ab9b4adf092d7
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2024-07-01 09:27:59 +0300

    Add script for downloading necessary data

---
+++ download_data.sh
@@ -0,0 +1,8 @@
+
+mkdir -p data
+
+gdown 1hDXCuVSfFXQiQVDuItAihd9Em4j3KPiX -O data/2010.txt # 2010.txt
+gdown 1SNW8M47De5EirKyDyCg6YD46D97CppVq -O data/conceptualization.txt # conceptualization.txt
+gdown 1G5ztOLdp165hGpbANoq2JGAugeM29Klu -O data/ate_stopwords.csv # ate_stopwords.csv
+gdown 1UhjBKslpcQsLjAywp-Wkn13yMsWvWTpr -O data/config.ini # config.ini
+gdown 1UYN3aEJHX-PstwxkGmpuQ4SLrLL4vC1c -O data/noisy_terms.txt # noisy_terms.txt
